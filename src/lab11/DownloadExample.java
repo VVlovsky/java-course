f07af2d48b54bd387f207fce6ced2ef3b40a6203
vvlovsky <slvtrshkv@gmail.com>
2020-01-14 23:18:43 +0100

    lab12 + lab11.DownloadExample

--- src/lab11/DownloadExample.java
+++ src/lab11/DownloadExample.java
@@ -1,9 +1,16 @@
 package lab11;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class DownloadExample {
 
     // lista plików do pobrania
-    static String [] toDownload = {
+    static String[] toDownload = {
             "http://home.agh.edu.pl/pszwed/wyklad-c/01-jezyk-c-intro.pdf",
             "http://home.agh.edu.pl/~pszwed/wyklad-c/02-jezyk-c-podstawy-skladni.pdf",
             "http://home.agh.edu.pl/~pszwed/wyklad-c/03-jezyk-c-instrukcje.pdf",
@@ -18,4 +25,96 @@ public class DownloadExample {
             "http://home.agh.edu.pl/~pszwed/wyklad-c/preprocesor-make-funkcje-biblioteczne.pdf",
     };
 
+    static AtomicInteger counter = new AtomicInteger(0);
+    static Semaphore semaphore = new Semaphore(0);
+
+
+    static class Downloader implements Runnable {
+        private final int type;
+        private final String url;
+
+        /**
+         * @param url  - file to download
+         * @param type - 0 - sequential, 1 - atomic lock, 2 - semaphore
+         */
+        Downloader(String url, int type) {
+            this.url = url;
+            this.type = type;
+        }
+
+        public void run() {
+            int data;
+            String fileName = getFileNameFromURL(url);
+
+            try (InputStream in = new URL(url).openStream();
+                 FileOutputStream out = new FileOutputStream(fileName)) {
+
+                while ((data = in.read()) != -1) {
+                    out.write(data);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (type == 1) {
+                counter.incrementAndGet();
+            } else if (type == 2) {
+                semaphore.release();
+            }
+
+            System.out.println(type + "Downloaded: " + fileName);
+        }
+
+        private String getFileNameFromURL(String url) {
+            return url.substring(url.lastIndexOf('/') + 1, url.length());
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Sequential download");
+        sequentialDownload();
+
+        System.out.println("Concurrent download v1");
+        concurrentDownloadV1();
+
+        System.out.println("Concurrent download v2");
+        concurrentDownloadV2();
+
+        System.out.println("Concurrent download v3");
+        concurrentDownloadV3();
+    }
+
+    static void sequentialDownload() {
+
+        for (String url : toDownload) {
+            new DownloadExample.Downloader(url, 0).run();
+        }
+    }
+
+    static void concurrentDownloadV1() {
+        for (String url : toDownload) {
+            new Thread(new DownloadExample.Downloader(url, 0)).start();
+        }
+
+    }
+
+    static void concurrentDownloadV2() {
+        for (String url : toDownload) {
+            new Thread(new DownloadExample.Downloader(url, 1)).start();
+        }
+
+        while (counter.get() != toDownload.length) {
+            Thread.yield();
+        }
+        counter.set(0);
+
+    }
+
+    static void concurrentDownloadV3() throws InterruptedException {
+        for (String url : toDownload) {
+            new Thread(new DownloadExample.Downloader(url, 2)).start();
+        }
+        semaphore.acquire(toDownload.length);
+    }
 }
---
+++ src/lab12/Mean.java
@@ -0,0 +1,89 @@
+package lab12;
+
+import java.util.Locale;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Mean {
+    static double[] array;
+    static int arrLength;
+    static BlockingQueue<Double> results = new ArrayBlockingQueue<Double>(100);
+
+    static void initArray(int size) {
+        array = new double[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = Math.random() * size / (i + 1);
+        }
+        arrLength = size;
+    }
+
+    static class MeanCalc extends Thread {
+        private final int start;
+        private final int end;
+        double mean = 0;
+
+        MeanCalc(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public void run() {
+            double sum = 0;
+            for (int i = start; i < end; i++) {
+                sum = sum + (array[i] / 10000);
+            }
+            mean = (sum / (end - start)) * 10000;
+
+            System.out.printf(Locale.US, "%d-%d mean=%f\n", start, end, mean);
+            try {
+                results.put(mean);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }// ??? liczymy średnią
+
+    /**
+     * Oblicza średnią wartości elementów tablicy array uruchamiając równolegle działające wątki.
+     * Wypisuje czasy operacji
+     *
+     * @param cnt - liczba wątków
+     */
+    static void parallelMean(int cnt) throws InterruptedException {
+        // utwórz tablicę wątków
+        MeanCalc threads[] = new MeanCalc[cnt];
+        // utwórz wątki, podziel tablice na równe bloki i przekaż indeksy do wątków
+        // załóż, że array.length dzieli się przez cnt)
+        for (int i = 0; i < cnt; i++) {
+            threads[i] = new MeanCalc(arrLength / cnt * i, arrLength / cnt * (i + 1));
+        }
+        double t1 = System.nanoTime() / 1e6;
+        //uruchom wątki
+        for (MeanCalc mc : threads) {
+            mc.start();
+        }
+        double t2 = System.nanoTime() / 1e6;
+        // czekaj na ich zakończenie używając metody ''join''
+
+        double sum = 0;
+        for (int i = 0; i < cnt; i++) {
+            sum = sum + results.take();
+        }
+        double mean = sum / cnt;
+        double t3 = System.nanoTime() / 1e6;
+        System.out.printf(Locale.US, "size = %d cnt=%d >  t2-t1=%f t3-t1=%f mean=%f\n",
+                array.length,
+                cnt,
+                t2 - t1,
+                t3 - t1,
+                mean);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        initArray(128000000);
+        for (int cnt : new int[]{1, 2, 4, 6, 8, 16, 32, 64, 128}) {
+            parallelMean(cnt);
+        }
+    }
+
+}
