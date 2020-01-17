7c4e1ce58a54ae0732985cfd4e689fd0719d3051
vvlovsky <slvtrshkv@gmail.com>
2020-01-17 16:34:24 +0100

    lab6 CSVReader full basic implementation

--- src/lab6/CSVReader.java
+++ src/lab6/CSVReader.java
@@ -1,6 +1,7 @@
 package lab6;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -14,11 +15,31 @@ public class CSVReader {
     Map<String, Integer> columnLabelsToInt = new HashMap<>();
     String[] current;
 
-    public CSVReader(String filename, String delimiter, boolean hasHeader) throws IOException {
-        reader = new BufferedReader(new FileReader(filename));
+    public CSVReader(String filename, String delimiter, boolean hasHeader) {
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         this.delimiter = delimiter;
         this.hasHeader = hasHeader;
-        if (hasHeader) parseHeader();
+
+        if (hasHeader) {
+            try {
+                parseHeader();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public CSVReader(String filename, String delimiter) {
+        this(filename, delimiter, false);
+    }
+
+    public CSVReader(String filename) {
+        this(filename, ";");
     }
 
 
@@ -27,8 +48,10 @@ public class CSVReader {
         if (line == null) {
             return;
         }
+
         String[] header = line.split(delimiter);
         for (int i = 0; i < header.length; i++) {
+            header[i] = header[i].trim();
             columnLabels.add(header[i]);
             columnLabelsToInt.put(header[i], i);
         }
@@ -40,18 +63,11 @@ public class CSVReader {
         if (line == null) {
             return false;
         }
-        String[] tmpCurrent = line.split(delimiter);
-        int addCounter = 0;
-        while (tmpCurrent.length + addCounter <= columnLabels.size()) {
-            addCounter++;
+
+        current = line.split(delimiter);
+        for (int i = 0; i < current.length; i++) {
+            current[i] = current[i].trim();
         }
-//        int addCounter = columnLabels.size() - tmpCurrent.length;
-        List<String> tmp = new ArrayList<String>(Arrays.asList(tmpCurrent));
-        for (int i = 0; i < addCounter; i++){
-            tmp.add("");
-        }
-        current = tmp.toArray(new String[tmp.size()]);
-//        current = line.split(delimiter);
         return true;
     }
 
@@ -73,10 +89,25 @@ public class CSVReader {
 
     public String get(int index) {
         return current[index];
-
     }
 
     public String get(String column) {
         return current[columnLabelsToInt.get(column)];
+    }
+
+    List<String> getColumnLabels() {
+        return columnLabels;
+    }
+
+    int getRecordLength() {
+        return current.length;
+    }
+
+    boolean isMissing(int columnIndex) {
+        return columnIndex > current.length - 1 || current[columnIndex].equals("");
+    }
+
+    boolean isMissing(String columnLabel) {
+        return current[columnLabelsToInt.get(columnLabel)].equals("");
     }
 }
--- src/lab6/TestCSV.java
+++ src/lab6/TestCSV.java
@@ -6,11 +6,11 @@ import java.util.Locale;
 public class TestCSV {
 
     public static void main(String[] args) throws IOException {
-        CSVReader reader = new CSVReader("/home/vvlovsky/Work/JavaProjects/JavaLabs/src/lab6/with-header.csv", ";", true);
+        CSVReader reader = new CSVReader("/home/vvlovsky/Work/JavaProjects/JavaLabs/src/lab6/csv_examples/missing-values.csv", ";", true);
         while (reader.next()) {
-            int id = reader.getInt("id");
-            String name = reader.get("nazwisko");
-            System.out.printf("%d %s", id, name);
+            int id = reader.getInt("population");
+//            String name = reader.get("nazwisko");
+            System.out.printf("%d", id);
             System.out.println();
         }
 
